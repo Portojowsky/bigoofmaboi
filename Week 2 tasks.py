02505a77c43108bff4b7e93f87ca90782fe95af6
Portojowsky <43777859+Portojowsky@users.noreply.github.com>
2018-10-09 10:47:29 +0100

    Add files via upload

---
+++ Week 2 tasks.py
@@ -0,0 +1,20 @@
+#ISD
+#Week 2
+
+#Task 3
+
+print("Hello World")
+
+#Task 4
+
+print("Hello World")
+print("I am in my ISD class right now")
+
+
+#Task 5
+
+print("Hi there, what is your name?")
+name = input("Insert name here ")
+print("Hello")
+print(name)
+print("How are you?")
